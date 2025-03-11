2a81b8be816b540eaf4675a6ccfbbaa0656fb964
Mylavarapu Sarath Sai <msarathsai@proton.me>
2025-03-11 21:03:18 +0530

    user table migration

---
+++ cmd/migrate/migrations/000001_create_users_table.up.sql
@@ -0,0 +1,11 @@
+CREATE EXTENSION IF NOT EXISTS citext;
+CREATE TABLE IF NOT EXISTS users (
+    id BIGSERIAL PRIMARY KEY,
+    username VARCHAR(30) NOT NULL,
+    email citext NOT NULL,
+    hashed_password bytea NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    updated_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
+);
